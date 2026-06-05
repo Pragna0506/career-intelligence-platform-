c0e96aaaa120caa66a8375f4774e6ba8ad0f58ba
Pragna S <pragnas0506@gmail.com>
2026-06-05 21:53:29 +0530

    Update insights.py

--- utils/insights.py
+++ utils/insights.py
@@ -1,1 +1,28 @@
+import pandas as pd
 
+
+# =====================================================
+# SALARY INSIGHTS
+# =====================================================
+
+def get_salary_insights(df):
+
+    insights = []
+
+    if "Starting_Salary" not in df.columns:
+        return ["Salary column not found."]
+
+    avg_salary = df["Starting_Salary"].mean()
+    max_salary = df["Starting_Salary"].max()
+    min_salary = df["Starting_Salary"].min()
+
+    insights.append(
+        f"Average salary is ₹{avg_salary:,.0f}"
+    )
+
+    insights.append(
+        f"Highest salary recorded is ₹{max_salary:,.0f}"
+    )
+
+    insights.append(
+        f"Lowest salary recorded is ₹
