fbc3c326d995fbfb3212a2c5041921c2a005d9df
Pragna S <pragnas0506@gmail.com>
2026-06-05 21:20:47 +0530

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -1,1 +1,7 @@
+import pandas as pd
 
+career = pd.read_csv("data/education_career_success.csv")
+print(career.columns.tolist())
+
+placement = pd.read_csv("data/placement.csv")
+print(placement.columns.tolist())
