3ed98dd0d6e73d1c9326346e66e6c26fefadb790
Pragna S <pragnas0506@gmail.com>
2026-06-06 08:17:08 +0530

    Update 7_Alumni_Tracker.py

--- pages/7_Alumni_Tracker.py
+++ pages/7_Alumni_Tracker.py
@@ -21,7 +21,7 @@ st.set_page_config(
 @st.cache_data
 def load_data():
     return pd.read_csv(
-        "data/education_career_success.csv"
+        "data3/education_career_success.csv"
     )
 
 df = load_data()
