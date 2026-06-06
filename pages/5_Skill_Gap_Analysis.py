8d926392eac0162d11d3bc695796d4cafbeaeaf1
Pragna S <pragnas0506@gmail.com>
2026-06-06 12:51:21 +0530

    Update 5_Skill_Gap_Analysis.py

--- pages/5_Skill_Gap_Analysis.py
+++ pages/5_Skill_Gap_Analysis.py
@@ -12,7 +12,7 @@ st.title("📊 Skill Gap Analysis Dashboard")
 # -----------------------------
 @st.cache_data
 def load_data():
-    file_path = "data/Job_Placement_Data_Enhanced.csv"
+    file_path = "data/job_Placement.csv"
 
     # ✅ CHECK IF FILE EXISTS
     if not os.path.exists(file_path):
